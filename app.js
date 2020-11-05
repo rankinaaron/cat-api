5332da6d71aa9160b291011370cb06957cdddbc8
Aaron Rankin <rankinaaron@gmail.com>
2020-11-05 17:39:21 -0600

    commented out magic lines

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const voteRoute = require('./routes/votes');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
 
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 app.use(logger('dev'));
 app.use(express.json());
