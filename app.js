00b792bfe36c65de7009e309785e44373012f897
Aaron Rankin <rankinaaron@gmail.com>
2020-11-05 17:29:21 -0600

    reverted previous changes

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const voteRoute = require('./routes/votes');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
